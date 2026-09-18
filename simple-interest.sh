f7eacd0805ac9fcb5f777fc4e346367ab4dfbf0f
Avdoot-pune <avdoot.punekar@gmail.com>
2026-09-19 02:01:25 +0530

    Fix typo

--- simple-interest.sh
+++ simple-interest.sh
@@ -13,7 +13,7 @@
 
 # Output:
 # simple interest = p*t*r
-
+# fixed some errors
 echo "Enter the principal:"
 read p
 echo "Enter rate of interest per year:"
@@ -24,3 +24,4 @@ read t
 s=$(expr $p \* $t \* $r / 100)
 echo "The simple interest is: "
 echo $s
+
